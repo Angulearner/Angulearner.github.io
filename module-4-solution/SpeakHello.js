794b0a0b3e3941450a339f40ed059c91fbc3d5bf
Angulearner <clem2010@live.com>
2016-08-24 23:39:25 -0500

    Update SpeakHello.js

--- module-4-solution/SpeakHello.js
+++ module-4-solution/SpeakHello.js
@@ -1,4 +1,4 @@
-(function() {
+(function(window) {
   
   // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
   // See Lecture 52, part 2
@@ -26,5 +26,5 @@
   // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
   window.helloSpeaker = helloSpeaker;
 
-})();
+})(window);
 
