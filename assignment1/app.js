f5d6c0a67713360367d384e11ef464cc5c0597a3
Angulearner <clem2010@live.com>
2016-09-10 12:04:26 -0500

    Update app.js

--- assignment1/app.js
+++ assignment1/app.js
@@ -8,6 +8,22 @@
    LunchCheckController.$inject = ['$scope'];
    function LunchCheckController ($scope)
    {
-     
+      $scope.lunchMessage = "";
+      
+      $scope.getMessage = function ()
+      {
+         var lunchText = String($scope.lunchText).trim();
+         if (lunchText != "")
+         {   
+            var lunchArray = lunchText.split(",");
+            if (lunchArray.length) <= 3
+               $scope.lunchMessage = "Enjoy!";
+            else
+               $scope.lunchMessage = "Too much!";
+        }
+        else
+         $scope.lunchMessage = "Please enter data first!";      
+      }
+   
    }
 })();
