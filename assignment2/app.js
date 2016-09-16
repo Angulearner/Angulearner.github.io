d5ff0be3e582da57fabdfe50363ee8a346fd6112
Angulearner <clem2010@live.com>
2016-09-15 23:48:24 -0500

    Update app.js

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,7 +1,7 @@
 (function(){
    "use strict";
    
-   angular.module('ShoppingListCheckoff', [])
+   angular.module('ShoppingListCheckOff', [])
    .controller ('ToBuyShoppingController', ToBuyShoppingController)
    .controller ('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
