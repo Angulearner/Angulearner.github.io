f8c962576ccd28fd2644a0571cb9648a450edf9f
Angulearner <clem2010@live.com>
2016-09-15 21:41:11 -0500

    Update app.js

--- assignment2/app.js
+++ assignment2/app.js
@@ -1,1 +1,29 @@
-
+(function(){
+   "use strict";
+   
+   angular.module('Shopping', [])
+   .controller ('LunchCheckController', LunchCheckController);
+   
+   LunchCheckController.$inject = ['$scope'];
+   function LunchCheckController ($scope)
+   {
+      $scope.lunchMessage = "";
+      
+      $scope.getMessage = function ()
+      {
+         var lunchText = String($scope.lunchText).trim();
+         
+         if (lunchText && lunchText != "undefined")
+         {   
+            var lunchArray = lunchText.split(",");
+            if (lunchArray.length <= 3)
+               $scope.lunchMessage = "Enjoy!";
+            else
+               $scope.lunchMessage = "Too much!";
+        }
+        else
+         $scope.lunchMessage = "Please enter data first!";      
+      }
+   
+   }
+})();
