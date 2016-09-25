4c6eaf53f13917e5a73de12f3e96b9b8cfc71fc4
Angulearner <clem2010@live.com>
2016-09-25 13:09:28 -0500

    Update menusearch.service.js

--- assignment3/menusearch.service.js
+++ assignment3/menusearch.service.js
@@ -13,7 +13,7 @@ angular.module('NarrowItDownApp')
             .then(function (result) 
             {
                 // process result and only keep items that match
-                var foundItems = result; // todo finish this!
+                var foundItems = result.data; // todo finish this!
 
               // return processed items
               console.log("found items = " + foundItems);
