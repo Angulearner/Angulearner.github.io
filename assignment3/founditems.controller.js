0009e35a4058021fa31fd79b5325f45a7d86eef7
Angulearner <clem2010@live.com>
2016-09-24 19:02:41 -0500

    Update founditems.controller.js

--- assignment3/founditems.controller.js
+++ assignment3/founditems.controller.js
@@ -1,1 +1,20 @@
+(function(){
 
+angular.module('NarrowItDownApp')
+  .controller('FoundItemsComponentController', FoundItemsComponentController);
+  
+  
+   FoundItemsComponentController.$inject = [];
+   function FoundItemsComponentController ()
+   {
+      var self = this;
+     
+      
+   }
+
+
+
+
+
+
+})();
