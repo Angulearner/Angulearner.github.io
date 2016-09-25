174f2f1b8f2794a2f717fe855c9308da020f997b
Angulearner <clem2010@live.com>
2016-09-24 20:32:16 -0500

    Update menusearch.service.js

--- assignment3/menusearch.service.js
+++ assignment3/menusearch.service.js
@@ -7,18 +7,16 @@ angular.module('NarrowItDownApp')
    {
       function getMatchedMenuItems(searchTerm)
       {
-        $http ({ 
-            method: 'GET',
-            url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
-        })
-        .then(function (result) 
-        {
-              // process result and only keep items that match
-              var foundItems = result; // todo finish this!
+          var promise = $http ('https://davids-restaurant.herokuapp.com/menu_items.json'); 
+         
+          promise.then(function (result) 
+          {
+                // process result and only keep items that match
+                var foundItems = result; // todo finish this!
 
-            // return processed items
-            return foundItems;
-          });         
+              // return processed items
+              return foundItems;
+          }).catch(function(error) { console.log ("problem!!");  });         
        
       }
       
