26e03394ec523509008242116b0513b06b1b6c24
Angulearner <clem2010@live.com>
2016-09-25 13:45:24 -0500

    Update founditems.directive.js

--- assignment3/founditems.directive.js
+++ assignment3/founditems.directive.js
@@ -1,1 +1,42 @@
+(function(){
 
+angular.module('NarrowItDownApp')
+  .directive('foundItems', foundItems);
+  
+  function foundItems()
+  {
+      var ddo =  
+      {
+          templateUrl: 'listFoundItems.html'
+      };
+    
+      return ddo;
+  }
+  
+  /*
+    {
+      templateUrl: 'listFoundItems.html',
+      controller: FoundItemsComponentController,
+      bindings:
+      {
+          found: '<',
+          onRemove: '&'
+      }
+    });
+
+   FoundItemsComponentController.$inject = [];
+   function FoundItemsComponentController ()
+   {
+      var self = this;
+     
+     self.remove = function(myIndex) 
+     {
+       self.onRemove({ index: myIndex})
+     }
+      
+   }
+*/
+
+
+
+})();
