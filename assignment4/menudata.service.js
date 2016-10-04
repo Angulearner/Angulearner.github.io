c6d3b31b6afaf45a7674eded53395f27e863c4b2
Angulearner <clem2010@live.com>
2016-10-03 22:22:32 -0500

    Update menudata.service.js

--- assignment4/menudata.service.js
+++ assignment4/menudata.service.js
@@ -1,1 +1,24 @@
-
+(function(){
+  
+  angular.module('data')
+    .service('menuDataService', MenuDataService);
+  
+  MenuDataService.$inject = ['$http'];
+  function MenuDataService($http)
+  {
+    self = this;
+    
+    self.getAllCategories = function()
+    {
+      
+    };
+    
+    self.getItemsForCategory = function(categoryShortName)
+    {
+      
+      
+    };
+    
+  };
+  
+})();
