58db97168a03aa41f1784348a614b42f2e6bb078
Angulearner <clem2010@live.com>
2016-10-03 22:34:22 -0500

    Update menudata.service.js

--- assignment4/menudata.service.js
+++ assignment4/menudata.service.js
@@ -10,12 +10,14 @@
     
     self.getAllCategories = function()
     {
-      
+        return $http.get('https://davids-restaurant.herokuapp.com/categories.json');
     };
     
     self.getItemsForCategory = function(categoryShortName)
     {
+        var url = "https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName;
       
+        return $http.get(url);
       
     };
     
