5946cd39e058d3dae330cb5843331a4d4c65cff9
Angulearner <clem2010@live.com>
2016-10-03 23:14:12 -0500

    Update categories.controller.js

--- assignment4/categories.controller.js
+++ assignment4/categories.controller.js
@@ -1,1 +1,17 @@
+(function () {
+'use strict';
 
+angular.module('MenuApp')
+.controller('CategoriesController', CategoriesController);
+
+
+// MainShoppingListController.$inject = ['ShoppingListService'];
+// function MainShoppingListController(ShoppingListService) {
+CategoriesController.$inject = ['categories'];
+function CategoriesController(categories) {
+  var self = this;
+  self.categories = categories;
+
+}
+
+})();
