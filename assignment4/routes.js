09bf42d5e5f1f09f35f00ba0aca815bfe1b356ba
Angulearner <clem2010@live.com>
2016-10-04 11:21:22 -0500

    Update routes.js

--- assignment4/routes.js
+++ assignment4/routes.js
@@ -23,9 +23,9 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
   .state('categories', {
     url: '/categories',
     templateUrl: 'templates/categories.html',
-    controller: 'CategoriesController as catCtrl'
-/*
-   ,resolve: {
+    controller: 'CategoriesController as catCtrl',
+
+    resolve: {
       categories: 
         ['MenuDataService', 
               function (MenuDataService) 
@@ -33,7 +33,7 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
                   return MenuDataService.getAllCategories();
               }
         ]
-    } */
+    } 
   });
 }
 
