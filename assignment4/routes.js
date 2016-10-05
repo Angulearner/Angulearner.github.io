885b4e2fabf2fe1bbaa13f3ab48315f7bd33d920
Angulearner <clem2010@live.com>
2016-10-04 22:03:28 -0500

    Update routes.js

--- assignment4/routes.js
+++ assignment4/routes.js
@@ -47,7 +47,7 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
          ['$stateParams', 'MenuDataService',
             function ($stateParams, MenuDataService) 
             {
-              mainItemCtrl.setCategory($stateParams.categoryShortName);
+              // mainItemCtrl.setCategory($stateParams.categoryShortName);
               return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
            //   .then(function(items)
            //  {
