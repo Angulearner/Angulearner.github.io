4fe29499527a4fd10bcc2268d96c47b8fae4a9fc
Angulearner <clem2010@live.com>
2016-10-04 21:03:03 -0500

    Update items.component.js

--- assignment4/items.component.js
+++ assignment4/items.component.js
@@ -3,7 +3,7 @@
   angular.module('MenuApp')
     .component('items', 
     {
-        templateUrl: 'templates/items.template.html',
+        templateUrl: 'templates/menu-items.template.html',
         bindings:
         {
             menuItems: '<'
