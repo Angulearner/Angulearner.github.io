edbd63c9292a7502298833caecf67d5de1254087
Angulearner <clem2010@live.com>
2016-10-04 22:38:31 -0500

    Update menudata.service.js

--- assignment4/menudata.service.js
+++ assignment4/menudata.service.js
@@ -22,13 +22,7 @@
         var url = "https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName;
       
         var prom =  $http.get(url);
-        prom.categoryShortName = categoryShortName;
-        for (var x in prom)
-        {
-            console.log(x);
-        }
         
-      
         return prom;
       
       
